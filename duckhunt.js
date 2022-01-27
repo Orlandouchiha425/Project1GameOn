c0f03d1f063bf05534d6c0dcc9bb805a3aa35f0b
orlandouchiha425 <valadez425@gmail.com>
2022-01-27 08:07:29 -0500

    Wednesday update

--- duckhunt.js
+++ duckhunt.js
@@ -10,16 +10,19 @@ const circle2 = document.getElementById('circle2');
 const circle3 = document.getElementById('circle3');
 const circle4 = document.getElementById('circle4');
 const circle5 = document.getElementById('circle5');
-
+const endGameButton=document.getElementById('endGame');
+const restartButton=document.getElementById('restart');
+const scoreBoard=document.getElementById('scoreboard');
 
 class Player { 
     constructor(points){
         this.points=points;
+        
     }
 
      hit(params) {
-         this.points++
-        console.log("HEY",this.points)
+        this.points++;
+        console.log("your current score is "+ this.points)
     }
 }
 
@@ -42,11 +45,11 @@ closeHowTo.addEventListener('click',(evt)=>{
 })
 
 
-function drawScore() {
-    ctx.font = "16px Arial";
-    ctx.fillStyle = "#0095DD";
-    ctx.fillText("Score: "+score, 8, 20);
-}
+// function drawScore() {
+//     ctx.font = "16px Arial";
+//     ctx.fillStyle = "#0095DD";
+//     ctx.fillText("Score: "+score, 8, 20);
+// }
 // https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Track_the_score_and_win
 
 
@@ -59,54 +62,110 @@ onePlayer.addEventListener('click',(evt)=>{
     circle3.style.display='block';
     circle4.style.display='block';
     circle5.style.display='block';
+    endGameButton.style.display='block';
+    restartButton.style.display='block';
+    
     allCircles()
-
-
     
 })
+
+
+
+
+const newTotal=()=>{
+
+
+    player1.points++
+    scoreBoard.textContent="your current score is " + player1.points;
+    if (player1.points===2) {
+        
+        console.log('you won');
+        window.alert("you won");
+        window.confirm("Do you want to end Game?");
+        location.reload()
+        }
+        
+    }
+
+
+    setTimeout(newTotal,3000,"Time is up")
+
 const allCircles=()=>{
 
+
+
+
     circle1.addEventListener('click',(evt)=>{
-        player1.hit()
-    //     if (evt.target===circle1) {
-    //         console.log('Hit')
+       
+        if (evt.target===circle1) {
             
-    //     }
-    // })
+         newTotal()
+        }
+    })
     
-    // circle2.addEventListener('click',(evt)=>{
-    //     if (evt.target===circle2) {
-    //         console.log('Hit')
-            
-    //     }
-    // })
+    circle2.addEventListener('click',(evt)=>{
+        if (evt.target===circle2) {
+            console.log('Hit')
+           
+            newTotal()
+                
+            }
+        }
+    )
     
     
-    // circle3.addEventListener('click',(evt)=>{
-    //     if (evt.target===circle3) {
-    //         console.log('Hit')
+    circle3.addEventListener('click',(evt)=>{
+        if (evt.target===circle3) {
+            console.log('Hit')
             
-    //     }
-    // })
-    // circle4.addEventListener('click',(evt)=>{
-    //     if (evt.target===circle4) {
-    //         console.log('Hit')
+            newTotal()
+            }
             
-    //     }
-    // })
+        
+    })
+    circle4.addEventListener('click',(evt)=>{
+        if (evt.target===circle4) {
+            console.log('Hit')
+            
+            newTotal() 
+        }
+    })
     
-    // circle5.addEventListener('click',(evt)=>{
-    //     if (evt.target===circle5) {
-    //         console.log('Hit')
-            
-    //     }
+    circle5.addEventListener('click',(evt)=>{
+        if (evt.target===circle5) {
+            console.log('Hit')
+            player1.points++
+            scoreBoard.textContent="your current score is " + player1.points;
+            // if (player1.points===5) {
+            //     console.log('you won')
+                
+            // }
+            newTotal()
+        }
     })
     
     goldenSnitch.addEventListener('click',(evt)=>{
         if (evt.target===goldenSnitch) {
             console.log("you won")
-        
+            
             
         }
     })
+        
+    
 }
+
+
+
+endGameButton.addEventListener('click',()=>{
+    if(window.confirm("Do you want to end Game?")){
+        window.close("GoodBye")
+    }
+    })
+restartButton.addEventListener('click',()=>{
+
+        location.reload()
+        //credit goes to damon, he gave me the idea during office hours.
+    
+    } )
+// window.alert('time is up')
