932693629011281d7329d97197439ab00beb51af
orlandouchiha425 <valadez425@gmail.com>
2022-01-30 20:07:14 -0500

    Sunday30th

--- duckhunt.js
+++ duckhunt.js
@@ -3,7 +3,7 @@ const twoPlayers=document.querySelector('.flex-container>.item2');//2Players
 const howTo=document.querySelector('.flex-container>.item3');//button How to play
 const openHowTo=document.getElementById('open');//opens how to instructions,need link with button how to play
 const closeHowTo=document.querySelector('#open>.close');//closes the button by removing const openHowTo
-const goldenSnitch=document.getElementById('goldensnitch');
+const playerTwoBanner=document.getElementById('playertwo');
 // const circleTargets=document.getElementById('targets');
 const circle1 = document.getElementById('circle1');
 const circle2 = document.getElementById('circle2');
@@ -12,11 +12,39 @@ const circle4 = document.getElementById('circle4');
 const circle5 = document.getElementById('circle5');
 const endGameButton=document.getElementById('endGame');
 const restartButton=document.getElementById('restart');
-const scoreBoard=document.getElementById('scoreboard');
-const pause=document.getElementsByClassName('pause');
+const playButton=document.getElementById('playButton');
+// const timer=document.getElementById("countdown");
+let scoreBoard=document.getElementById('scoreboard');
+const playerTwoButton=document.getElementById('playerTwoButton');
+const player2ScoreBoard=document.getElementById('player2Scoreboard');
 
 
-let score=[];
+
+let counter=0;
+
+let timer
+// const startingTime=10;
+// let decreasingTime=startingTime;
+
+
+
+// const countDown=()=>{
+//     let seconds=decreasingTime;
+//     decreasingTime --;
+//     timer.textContent=seconds;
+//     if (timer===0) {
+//         window.alert('gameover')
+        
+//     }
+    
+    
+
+
+let scorePlayerOne=0;
+let scorePlayerTwo=0;
+
+
+
 
 class Player { 
     constructor(points){
@@ -24,16 +52,13 @@ class Player {
         
     }
 
-    //  hit(params) {
-    //     this.points++;
-    //     console.log("your current score is "+ this.points)
-    // }
+   
 }
 
 const player1=new Player(0);
 const player2=new Player(0)
-
-
+// let currentPlayer=player1;
+// let currentPlayer2=player2;
 
 
 
@@ -48,12 +73,7 @@ closeHowTo.addEventListener('click',(evt)=>{
 })
 
 
-// function drawScore() {
-//     ctx.font = "16px Arial";
-//     ctx.fillStyle = "#0095DD";
-//     ctx.fillText("Score: "+score, 8, 20);
-// }
-// https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Track_the_score_and_win
+
 
 
 onePlayer.addEventListener('click',(evt)=>{
@@ -67,41 +87,173 @@ onePlayer.addEventListener('click',(evt)=>{
     circle5.style.display='block';
     endGameButton.style.display='block';
     restartButton.style.display='block';
-    
     allCircles()
+    
+    
     
 })
 
 
-twoPlayers.addEventListener('click',(evt)=>{
+twoPlayers.addEventListener('click',()=>{
     onePlayer.style.display='none';
     twoPlayers.style.display='none';
     howTo.style.display='none';
+    playButton.style.display="block";
+    
+
+playButton.addEventListener('click',(evt)=>{
+
+        circle1.style.display='block';
+        circle2.style.display='block';
+        circle3.style.display='block';
+        circle4.style.display='block';
+        circle5.style.display='block';
+        endGameButton.style.display='block';
+        restartButton.style.display='block';
+        playButton.style.display="none"; 
+        firstPlayer()
+        timer = setInterval(() => {
+            counter++
+            if(counter > 5){
+                
+               clearInterval(timer)
+               playButton.style.display='none'
+               circle1.style.display='block';
+        circle2.style.display='none';
+        circle3.style.display='none';
+        circle4.style.display='none';
+        circle5.style.display='none';
+    playerTwoButton.style.display = "block";
+    console.log("Player two turn")
+            // window.stop()
+              
+            }
+          }, 1000)
+        
+        
+         
+    })
+   
+playerTwoButton.addEventListener('click',(evt)=>{
+    
+      
     circle1.style.display='block';
     circle2.style.display='block';
     circle3.style.display='block';
     circle4.style.display='block';
     circle5.style.display='block';
-    endGameButton.style.display='block';
-    restartButton.style.display='block';
     
-    allCircles()
-    
+    playButton.style.display="none"; 
+    playerTwoButton.style.display="none"
+    secondPlayer()
+    timer = setInterval(() => {
+        counter++
+        if(counter > 10){
+           clearInterval(timer)
+           console.log("time is up")
+            checkForWin()
+        }
+        
+    }, 1000)
+                
+           
+})
 })
 
 
+// playerTwoButton.addEventListener('click',(evt)=>{
+    
+      
+//     circle1.style.display='block';
+//     circle2.style.display='block';
+//     circle3.style.display='block';
+//     circle4.style.display='block';
+//     circle5.style.display='block';
+    
+//     playButton.style.display="none"; 
+//     playerTwoButton.style.display="none"
+//     secondPlayer()
+//     timer = setInterval(() => {
+//         counter++
+//         if(counter > 10){
+//            clearInterval(timer)
+//            console.log("time is up")
+//             checkForWin()
+//         }
+        
+//     }, 1000)
+                
+           
+// })
+
+
+
+
+const checkForWin=()=>{
+
+    if (scorePlayerOne>scorePlayerTwo) {
+        
+        console.log("Player one wins")
+        scoreBoard.textContent=" Player one wins"
+    }
+    if (scorePlayerOne===scorePlayerTwo) {
+        console.log("is a tie")
+        scoreBoard.textContent=" is a tie";
+        
+    }
+    if (scorePlayerOne<scorePlayerTwo) {
+        
+        console.log("Player two wins")
+        scoreBoard.textContent="Player two wins";
+    }
+}
 
 
 
 
 
-const newTotal=()=>{
-    let score=0;
 
+
+const playerOne=()=>{
     player1.points++
-    score+=player1.points;
-    console.log(score);
-    scoreBoard.textContent="your current score is " + score;
+    scorePlayerOne=player1.points;
+    console.log("player one score is "+scorePlayerOne)
+    
+    
+    scoreBoard.textContent="player one your current score is " + scorePlayerOne;
+    
+    return scorePlayerOne;
+    
+
+    
+}
+
+const playertwo=()=>{
+   
+    player2.points++
+    scorePlayerTwo=player2.points;
+    console.log("player two score is "+scorePlayerTwo)
+    
+    scoreBoard.textContent="Player Two current score is " + scorePlayerTwo;
+    return scorePlayerTwo;
+  
+   
+}
+
+
+
+
+
+
+
+
+const scorePlayerOneGame=()=>{
+    // countDown()
+    let scorePlayerOne=0
+    player1.points++
+       scorePlayerOne+=player1.points;
+       console.log(scorePlayerOne);
+       scoreBoard.textContent="your current score is " + scorePlayerOne;
     if (player1.points===3) {
         
         
@@ -126,15 +278,14 @@ const allCircles=()=>{
        
         if (evt.target===circle1) {
             
-         newTotal()
+        scorePlayerOneGame()
         }
     })
     
     circle2.addEventListener('click',(evt)=>{
         if (evt.target===circle2) {
-            console.log('Hit')
-           
-            newTotal()
+            
+            scorePlayerOneGame()
                 
             }
         }
@@ -145,7 +296,7 @@ const allCircles=()=>{
         if (evt.target===circle3) {
             console.log('Hit')
             
-            newTotal()
+            scorePlayerOneGame()
             }
             
         
@@ -154,7 +305,7 @@ const allCircles=()=>{
         if (evt.target===circle4) {
             console.log('Hit')
             
-            newTotal() 
+            scorePlayerOneGame() 
         }
     })
     
@@ -167,20 +318,122 @@ const allCircles=()=>{
             //     console.log('you won')
                 
             // }
-            newTotal()
-        }
-    })
-    
-    goldenSnitch.addEventListener('click',(evt)=>{
-        if (evt.target===goldenSnitch) {
-            console.log("you won")
-            
-            
+            scorePlayerOneGame()
         }
     })
         
    
 }
+
+
+
+const firstPlayer=()=>{
+        
+     circle1.addEventListener('click',(evt)=>{
+       
+        if (evt.target===circle1) {
+            
+            playerOne()
+        
+        }
+    })
+    
+    circle2.addEventListener('click',(evt)=>{
+        if (evt.target===circle2) {
+            
+            playerOne()        
+            }
+        }
+    )
+    
+    
+    circle3.addEventListener('click',(evt)=>{
+        if (evt.target===circle3) {
+            console.log('Hit')
+            
+            playerOne()    
+            }
+            
+        
+    })
+    circle4.addEventListener('click',(evt)=>{
+        if (evt.target===circle4) {
+            console.log('Hit')
+            
+            playerOne()
+        }
+    })
+    
+    circle5.addEventListener('click',(evt)=>{
+        if (evt.target===circle5) {
+            console.log('Hit')
+    
+            playerOne()  
+        }
+    })
+        
+   
+}
+
+
+
+
+
+
+
+
+
+
+const secondPlayer=()=>{
+
+    circle1.addEventListener('click',(evt)=>{
+       
+        if (evt.target===circle1) {
+            
+            playertwo()
+        
+        }
+    })
+    
+    circle2.addEventListener('click',(evt)=>{
+        if (evt.target===circle2) {
+            
+            playertwo()       
+            }
+        }
+    )
+    
+    
+    circle3.addEventListener('click',(evt)=>{
+        if (evt.target===circle3) {
+            console.log('Hit')
+            
+            pplayertwo()   
+            }
+            
+        
+    })
+    circle4.addEventListener('click',(evt)=>{
+        if (evt.target===circle4) {
+            console.log('Hit')
+            
+            playertwo()
+        }
+    })
+    
+    circle5.addEventListener('click',(evt)=>{
+        if (evt.target===circle5) {
+            console.log('Hit')
+    
+            playertwo() 
+        }
+    })
+}
+
+
+
+
+
 
 
 
@@ -195,4 +448,3 @@ restartButton.addEventListener('click',()=>{
         //credit goes to damon, he gave me the idea during office hours.
     
     } )
-// window.alert('time is up')
